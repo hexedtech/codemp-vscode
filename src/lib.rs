7f9422103a135031907ab18bab03e306695f5623
frelodev <frelodev@gmail.com>
2023-08-19 18:43:22 +0200

    feat: switched from neon to napi
    Co-authored-by: alemi <me@alemi.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,268 +1,177 @@
+#![deny(clippy::all)]
+
 use std::sync::Arc;
-
-use neon::prelude::*;
-use once_cell::sync::OnceCell;
+use futures::prelude::*;
+use napi::bindgen_prelude::*;
 use codemp::{
-	controller::cursor::{CursorSubscriber, CursorControllerHandle},
-	controller::buffer::{OperationControllerHandle, OperationControllerSubscriber},
-	client::CodempClient,
-	factory::OperationFactory,
-	proto::buffer_client::BufferClient, 
+	prelude::*,
+	proto::{RowCol, CursorEvent},
+	buffer::factory::OperationFactory, ot::OperationSeq
 };
-use codemp::tokio::{runtime::Runtime, sync::Mutex};
+use napi_derive::napi;
+use napi::tokio::{self, fs};
 
-fn runtime<'a, C: Context<'a>>(cx: &mut C) -> NeonResult<&'static Runtime> {
-	static RUNTIME: OnceCell<Runtime> = OnceCell::new();
+#[derive(Debug)]
+struct JsCodempError(CodempError);
 
-	RUNTIME.get_or_try_init(|| {
-		Runtime::new()
-			.or_else(|err| cx.throw_error(err.to_string()))
-	})
+impl From::<JsCodempError> for napi::Error {
+	fn from(value: JsCodempError) -> Self {
+		napi::Error::new(Status::GenericFailure, &format!("CodempError: {:?}", value))
+	}
 }
 
-fn tuple<'a, C: Context<'a>>(cx: &mut C, a: i32, b: i32) -> NeonResult<Handle<'a, JsArray>> {
-	let obj = cx.empty_array();
-	let a_val = cx.number(a);
-	obj.set(cx, 0, a_val)?;
-	let b_val = cx.number(b);
-	obj.set(cx, 1, b_val)?;
-	Ok(obj)
+#[napi]
+pub fn connect(addr: String) -> napi::Result<()> {
+	CODEMP_INSTANCE.connect(&addr)
+		.map_err(|e| JsCodempError(e).into())
 }
 
-fn unpack_tuple<'a, C: Context<'a>>(cx: &mut C, arr: Handle<'a, JsArray>) -> NeonResult<(i32, i32)> {
-	Ok((
-		arr.get::<JsNumber, _, u32>(cx, 0)?.value(cx) as i32,
-		arr.get::<JsNumber, _, u32>(cx, 1)?.value(cx) as i32,
-	))
+
+
+/// CURSOR
+
+#[napi]
+pub fn join(session: String) -> napi::Result<JsCursorController> {
+	let controller = CODEMP_INSTANCE.join(&session)
+		.map_err(|e| napi::Error::from(JsCodempError(e)))?;
+	Ok(controller.into())
 }
 
-struct ClientHandle(Arc<Mutex<CodempClient>>);
-impl Finalize for ClientHandle {}
+#[napi]
+pub struct JsCursorController(Arc<CodempCursorController>);
 
-fn connect(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let host = cx.argument::<JsString>(0).ok().map(|x| x.value(&mut cx));
+impl From::<Arc<CodempCursorController>> for JsCursorController {
+	fn from(value: Arc<CodempCursorController>) -> Self {
+		JsCursorController(value)
+	}
+}
 
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
+#[napi]
+impl JsCursorController {
 
-	runtime(&mut cx)?.spawn(async move {
-		match BufferClient::connect(host.unwrap_or("".into())).await {
-			Err(e) => deferred.settle_with(&channel, move |mut cx| cx.throw_error::<String, neon::handle::Handle<JsString>>(format!("{}", e))),
-			Ok(c) => deferred.settle_with(&channel, |mut cx| {
-				let obj = cx.empty_object();
-				let boxed_value = cx.boxed(ClientHandle(Arc::new(Mutex::new(c.into()))));
-				obj.set(&mut cx, "boxed", boxed_value)?;
-				let method_create = JsFunction::new(&mut cx, create_client)?;
-				obj.set(&mut cx, "create", method_create)?;
-				let method_listen = JsFunction::new(&mut cx, listen_client)?;
-				obj.set(&mut cx, "listen", method_listen)?;
-				let method_attach = JsFunction::new(&mut cx, attach_client)?;
-				obj.set(&mut cx, "attach", method_attach)?;
-				Ok(obj)
-			}),
+	#[napi]
+	pub async fn recv(&self) -> napi::Result<JsCursorEvent> {
+		Ok(
+			self.0.recv().await
+				.map_err(|e| napi::Error::from(JsCodempError(e)))?
+				.into()
+		)
+	}
+
+	#[napi]
+	pub async fn send(&self, buffer: String, start: (i32, i32), end: (i32, i32)) -> napi::Result<()> {
+		let pos = CodempCursorPosition { buffer, start: Some(RowCol::from(start)), end: Some(RowCol::from(end)) };
+		self.0.send(pos).await
+				.map_err(|e| napi::Error::from(JsCodempError(e)))
+	}
+}
+
+#[napi(object)]
+pub struct JsCursorEvent {
+	pub user: String,
+	pub buffer: String,
+	pub start: JsRowCol,
+	pub end: JsRowCol,
+}
+
+impl From::<CursorEvent> for JsCursorEvent {
+	fn from(value: CursorEvent) -> Self {
+		let pos = value.position.unwrap_or_default();
+		let start = pos.start.unwrap_or_default();
+		let end = pos.end.unwrap_or_default();
+		JsCursorEvent {
+			user: value.user,
+			buffer: pos.buffer,
+			start: JsRowCol { row: start.row, col: start.col },
+			end: JsRowCol { row: end.row, col: end.col },
 		}
-	});
-
-	Ok(promise)
+	}
 }
 
-fn create_client(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let path = cx.argument::<JsString>(0)?.value(&mut cx);
-	let content = cx.argument::<JsString>(1).ok().map(|x| x.value(&mut cx));
-	let this = cx.this();
-	let boxed : Handle<JsBox<ClientHandle>> = this.get(&mut cx, "boxed")?;
+#[napi(object)]
+pub struct JsRowCol {
+	pub row: i32,
+	pub col: i32
+}
 
-	let rc = boxed.0.clone();
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
+impl From::<RowCol> for JsRowCol {
+	fn from(value: RowCol) -> Self {
+		JsRowCol { row: value.row, col: value.col }
+	}
+}
 
-	runtime(&mut cx)?.spawn(async move {
-		match rc.lock().await.create(path, content).await {
-			Ok(accepted) => deferred.settle_with(&channel, move |mut cx| Ok(cx.boolean(accepted))),
-			Err(e) => deferred.settle_with(&channel, move |mut cx| cx.throw_error::<String, neon::handle::Handle<JsString>>(e.to_string())),
+
+
+/// BUFFER
+#[napi(object)]
+pub struct JsTextChange {
+	pub span: JSRange,
+	pub content: String,
+}
+#[napi(object)]
+pub struct JSRange{
+	pub start: i32,
+	pub end: Option<i32>,
+}
+
+impl From::<CodempTextChange> for JsTextChange {
+	fn from(value: CodempTextChange) -> Self {
+		JsTextChange {
+			// TODO how is x.. represented ? span.end can never be None
+			span: JSRange { start: value.span.start as i32, end: Some(value.span.end as i32) },
+			content: value.content,
 		}
-	});
-
-	Ok(promise)
+	}
 }
 
-fn listen_client(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<ClientHandle>> = this.get(&mut cx, "boxed")?;
-
-	let rc = boxed.0.clone();
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
-
-	runtime(&mut cx)?.spawn(async move {
-		match rc.lock().await.listen().await {
-			Ok(controller) => {
-				deferred.settle_with(&channel, move |mut cx| {
-					let obj = cx.empty_object();
-					let boxed_value = cx.boxed(CursorEventsHandle(controller));
-					obj.set(&mut cx, "boxed", boxed_value)?;
-					let callback_method = JsFunction::new(&mut cx, callback_cursor)?;
-					obj.set(&mut cx, "callback", callback_method)?;
-					let send_method = JsFunction::new(&mut cx, send_cursor)?;
-					obj.set(&mut cx, "send", send_method)?;
-					Ok(obj)
-				})
-			},
-			Err(e) => deferred.settle_with(&channel, move |mut cx| cx.throw_error::<String, neon::handle::Handle<JsString>>(e.to_string())),
-		}
-	});
-
-	Ok(promise)
-}
-
-fn attach_client(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<ClientHandle>> = this.get(&mut cx, "boxed")?;
-	let path = cx.argument::<JsString>(0)?.value(&mut cx);
-
-	let rc = boxed.0.clone();
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
-
-	runtime(&mut cx)?.spawn(async move {
-		match rc.lock().await.attach(path).await {
-			Ok(controller) => {
-				deferred.settle_with(&channel, move |mut cx| {
-					let obj = cx.empty_object();
-					let boxed_value = cx.boxed(OperationControllerJs(controller));
-					obj.set(&mut cx, "boxed", boxed_value)?;
-					let apply_method = JsFunction::new(&mut cx, apply_operation)?;
-					obj.set(&mut cx, "apply", apply_method)?;
-					let content_method = JsFunction::new(&mut cx, content_operation)?;
-					obj.set(&mut cx, "content", content_method)?;
-					let callback_method = JsFunction::new(&mut cx, callback_operation)?;
-					obj.set(&mut cx, "callback", callback_method)?;
-					Ok(obj)
-				})
-			},
-			Err(e) => deferred.settle_with(&channel, move |mut cx| cx.throw_error::<String, neon::handle::Handle<JsString>>(e.to_string())),
-		}
-	});
-
-	Ok(promise)
+impl From::<Arc<CodempBufferController>> for JsBufferController {
+	fn from(value: Arc<CodempBufferController>) -> Self {
+		JsBufferController(value)
+	}
 }
 
 
-struct OperationControllerJs(OperationControllerHandle);
-impl Finalize for OperationControllerJs {}
+#[napi]
+pub struct JsBufferController(Arc<CodempBufferController>);
 
-fn apply_operation(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<OperationControllerJs>> = this.get(&mut cx, "boxed")?;
-	let skip = cx.argument::<JsNumber>(0)?.value(&mut cx).round() as usize;
-	let text = cx.argument::<JsString>(1)?.value(&mut cx);
-	let tail = cx.argument::<JsNumber>(2)?.value(&mut cx).round() as usize;
 
-	let rc = boxed.0.clone();
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
+#[napi]
+impl JsBufferController {
 
-	runtime(&mut cx)?.spawn(async move {
-		if let Some(op) = rc.delta(skip, text.as_str(), tail) {
-			rc.apply(op).await;
-			deferred.settle_with(&channel, move |mut cx| Ok(cx.boolean(true)));
-		} else {
-			deferred.settle_with(&channel, move |mut cx| Ok(cx.undefined()));
-		}
-	});
+	#[napi]
+	pub async fn recv(&self) -> napi::Result<JsTextChange> {
+		Ok(
+			self.0.recv().await
+				.map_err(|e| napi::Error::from(JsCodempError(e)))?
+				.into()
+		)
+	}
 
-	Ok(promise)
+	//#[napi]
+	pub async fn send(&self, op: OperationSeq) -> napi::Result<()> {
+		self.0.send(op).await
+				.map_err(|e| napi::Error::from(JsCodempError(e)))
+	}
+
+
 }
 
-fn content_operation(mut cx: FunctionContext) -> JsResult<JsString> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<OperationControllerJs>> = this.get(&mut cx, "boxed")?;
-	Ok(cx.string(boxed.0.content()))
+#[napi]
+pub fn create(path: String, content: Option<String>) -> napi::Result<()> {
+	CODEMP_INSTANCE.create(&path, content.as_deref())
+		.map_err(|e| napi::Error::from(JsCodempError(e)))
 }
 
-fn callback_operation(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<OperationControllerJs>> = this.get(&mut cx, "boxed")?;
-	let callback = Arc::new(cx.argument::<JsFunction>(0)?.root(&mut cx));
-
-	let mut rc = boxed.0.clone();
-	let channel = cx.channel();
-
-	// TODO when garbage collecting OperationController stop this worker
-	runtime(&mut cx)?.spawn(async move {
-		while let Some(edit) = rc.poll().await {
-			let cb = callback.clone();
-			channel.send(move |mut cx| {
-				cb.to_inner(&mut cx)
-					.call_with(&cx)
-					.arg(cx.number(edit.span.start as i32))
-					.arg(cx.number(edit.span.end as i32))
-					.arg(cx.string(edit.content))
-					.apply::<JsUndefined, _>(&mut cx)?;
-				Ok(())
-			});
-		}
-	});
-
-	Ok(cx.undefined())
+#[napi]
+pub fn attach(path: String) -> napi::Result<JsBufferController> {
+	Ok(
+		CODEMP_INSTANCE.attach(&path)
+			.map_err(|e| napi::Error::from(JsCodempError(e)))?
+			.into()
+	)
 }
 
-struct CursorEventsHandle(CursorControllerHandle);
-impl Finalize for CursorEventsHandle {}
 
-fn callback_cursor(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<CursorEventsHandle>> = this.get(&mut cx, "boxed")?;
-	let callback = Arc::new(cx.argument::<JsFunction>(0)?.root(&mut cx));
 
-	let mut rc = boxed.0.clone();
-	let channel = cx.channel();
 
-	// TODO when garbage collecting OperationController stop this worker
-	runtime(&mut cx)?.spawn(async move {
-		while let Some(op) = rc.poll().await {
-			let cb = callback.clone();
-			channel.send(move |mut cx| {
-				cb.to_inner(&mut cx)
-					.call_with(&cx)
-					.arg(cx.string(&op.user))
-					.arg(cx.string(&op.buffer))
-					.arg(tuple(&mut cx, op.start().row, op.start().col)?)
-					.arg(tuple(&mut cx, op.end().row, op.end().col)?)
-					.apply::<JsUndefined, _>(&mut cx)?;
-				Ok(())
-			});
-		}
 
-	});
-
-	Ok(cx.undefined())
-}
-
-fn send_cursor(mut cx: FunctionContext) -> JsResult<JsPromise> {
-	let this = cx.this();
-	let boxed : Handle<JsBox<CursorEventsHandle>> = this.get(&mut cx, "boxed")?;
-	let path = cx.argument::<JsString>(0)?.value(&mut cx);
-	let start_obj = cx.argument::<JsArray>(1)?;
-	let start = unpack_tuple(&mut cx, start_obj)?;
-	let end_obj = cx.argument::<JsArray>(2)?;
-	let end = unpack_tuple(&mut cx, end_obj)?;
-
-	let rc = boxed.0.clone();
-	let (deferred, promise) = cx.promise();
-	let channel = cx.channel();
-
-	runtime(&mut cx)?.spawn(async move {
-		rc.send(&path, start.into(), end.into()).await;
-		deferred.settle_with(&channel, |mut cx| Ok(cx.undefined()))
-	});
-
-	Ok(promise)
-}
-
-#[neon::main]
-fn main(mut cx: ModuleContext) -> NeonResult<()> {
-	cx.export_function("connect", connect)?;
-
-	Ok(())
-}
