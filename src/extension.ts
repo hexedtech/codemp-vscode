fa6a0b23993b8629a4ee6bd31b41c2bf60a1ca93
frelodev <frelodev@gmail.com>
2024-02-14 15:41:32 +0100

    feat: Logger

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,15 @@ import * as vscode from 'vscode';
 import * as codemp from '../index'; // TODO why won't it work with a custom name???
 import * as codemplogic from './codemp';
 
+export let LOGGER = vscode.window.createOutputChannel("codemp", {log: true});
+
 // extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	console.log('Congratulations, your extension "codempvscode" is now active!');
 
 	// start codemp log poller
-	let channel = vscode.window.createOutputChannel("codemp", {log: true});
-	let logger = new codemp.JsLogger(false);
-	log_poller_task(logger, channel); // don't await it! run it in background forever
+	log_poller_task(new codemp.JsLogger(true)); // don't await it! run it in background forever
 
 	// register commands: the commandId parameter must match the command field in package.json
 	for (let cmd of [
@@ -22,18 +22,19 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('codempvscode.listBuffers', codemplogic.listBuffers),
 		// vscode.commands.registerCommand('codempvscode.disconnectBuffer', codemplogic.disconnectBuffer),
 		vscode.commands.registerCommand('codempvscode.sync', codemplogic.sync),
+		vscode.commands.registerCommand('codempvscode.printOpCache', codemplogic.printOpCache)
 	]) {
 		context.subscriptions.push(cmd);
 	}
 }
 
-async function log_poller_task(logger: codemp.JsLogger, channel: vscode.LogOutputChannel) {
+async function log_poller_task(logger: codemp.JsLogger) {
 	console.log("starting logger task");
 	while (true) {
 		let message = await logger.message();
 		if (message === null) break;
 		console.log(message);
-		channel.info(message);
+		LOGGER.info(message);
 	}
 	console.log("stopping logger task");
 }
